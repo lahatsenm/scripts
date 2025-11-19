94506df8e1ec52f2c01bb331b8ba03d487402854
lahatsenm <lahatseniavomilasoa@gmail.com>
2025-11-20 00:17:38 +0300

    update niri-manual-install-helper.sh

--- niri-manual-install-helper.sh
+++ niri-manual-install-helper.sh
@@ -3,10 +3,10 @@
 niri_build="$HOME/pkg_sources/niri"
 
 if [ -e "$niri_build" ]; then
-    sudo cp --verbose $niri_build/target/release/niri /usr/local/bin/
-    sudo cp --verbose $niri_build/resources/niri-session /usr/local/bin/
-    sudo cp --verbose $niri_build/resources/niri.desktop /usr/local/share/wayland-sessions/
-    sudo cp --verbose $niri_build/resources/niri-portals.conf /usr/local/share/xdg-desktop-portal/
+    sudo cp --verbose $niri_build/target/release/niri /usr/bin/
+    sudo cp --verbose $niri_build/resources/niri-session /usr/bin/
+    sudo cp --verbose $niri_build/resources/niri.desktop /usr/share/wayland-sessions/
+    sudo cp --verbose $niri_build/resources/niri-portals.conf /usr/share/xdg-desktop-portal/
     sudo cp --verbose $niri_build/resources/niri.service /etc/systemd/user/
     sudo cp --verbose $niri_build/resources/niri-shutdown.target /etc/systemd/user/
 else
